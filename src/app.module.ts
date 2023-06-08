e01f0af1a7923b432c723d6e2ca13d805ba4b147
Luiz Fernando Megale Paiva <luiz_paiva@gec.inatel.br>
2023-06-08 12:58:33 -0300

    alterando para livros.controller

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,11 +1,26 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { ProdutosController } from './produtos.controller';
+import { LivrosController } from './livros.controller';
+import { LivrosService } from './livros.service';
+import { SequelizeModule } from '@nestjs/sequelize';
+import { Livro } from './livro.model';
 
 @Module({
-  imports: [],
-  controllers: [AppController, ProdutosController],
-  providers: [AppService],
+  imports: [
+    SequelizeModule.forRoot({
+      dialect: 'mysql',
+      host: 'localhost',
+      port: 3306,
+      username: 'root',
+      password: '130594',
+      database: 'livraria_nestjs',
+      autoLoadModels: true,
+      synchronize: true,
+    }),
+    SequelizeModule.forFeature([Livro]),
+  ],
+  controllers: [AppController, LivrosController],
+  providers: [AppService, LivrosService],
 })
 export class AppModule {}
